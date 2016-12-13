2e5b5aefd4406418af0d084da35d8912bf25835f
Patrick Sturm <patrick.sturm@mail.de>
2016-12-14 00:32:57 +0100

    [impl] Implement cross product sequence

---
+++ Tests/CrossProductSequenceTests.swift
@@ -0,0 +1,52 @@
+/*
+ Copyright (c) 2016 Patrick Sturm <psturm.mail@googlemail.com>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+import XCTest
+@testable import FoundationTools
+
+class CrossProductSequenceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCrossProducOfTwoArrays() {
+        let a = [4, 5, 6]
+        let b = [1, 2]
+
+        let actual = Array(crossProduct(ofSequences: a, b))
+        let result = [(4, 1), (4, 2), (5, 1), (5, 2), (6, 1), (6, 2)]
+                
+        for i in 0..<min(actual.count, result.count) {
+            let actualItem = actual[i]
+            let resultItem = result[i]
+            XCTAssertEqual(resultItem.0, actualItem.0)
+            XCTAssertEqual(resultItem.1, actualItem.1)
+        }
+    }
+}
